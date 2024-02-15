aa61fd8434ad3598264132d2a8c5d794cb5aaac6
Acacio S. Costa <acacio@locadados.com>
2024-02-15 13:58:07 -0300

    complete first part

--- app/src/main/java/com/example/netflixremake/CategoryAdapter.kt
+++ app/src/main/java/com/example/netflixremake/CategoryAdapter.kt
@@ -3,17 +3,16 @@ package com.example.netflixremake
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.RecyclerView.LayoutManager
 import com.example.netflixremake.model.Category
-import com.example.netflixremake.model.Movie
 
-class CategoryAdapter(private val categories: List<Category>) : RecyclerView.Adapter<CategoryAdapter.CategoryViewHolder>() {
+class CategoryAdapter(private val categories: List<Category>) :
+    RecyclerView.Adapter<CategoryAdapter.CategoryViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.category_item, parent, false)
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.category_item, parent, false)
         return CategoryViewHolder(view)
     }
 
@@ -26,14 +25,15 @@ class CategoryAdapter(private val categories: List<Category>) : RecyclerView.Ada
         holder.bind(category)
     }
 
-    class CategoryViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+    class CategoryViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun bind(category: Category) {
             val txtTitle: TextView = itemView.findViewById(R.id.txt_title)
             val rvCategory: RecyclerView = itemView.findViewById(R.id.rv_category)
 
             txtTitle.text = category.name
 
-            rvCategory.layoutManager = LinearLayoutManager(itemView.context, RecyclerView.HORIZONTAL, false)
+            rvCategory.layoutManager =
+                LinearLayoutManager(itemView.context, RecyclerView.HORIZONTAL, false)
             rvCategory.adapter = MovieAdapter(category.movies)
         }
     }
